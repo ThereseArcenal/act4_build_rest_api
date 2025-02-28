9db0910495cc09e6a8f5e935dc06d23422cd0798
Therese Arcenal <galearcenal6@gmail.com>
2025-03-01 00:09:20 +0800

    Conflict

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
-import { userRouter } from "./users/user.routes";
+import { userRouter } from "./users/users.routes";
+import {productRouter} from "./products/product.routes";  
 
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(cors());
 app.use(helmet());
 
 app.use ('/', userRouter)
+app.use ('/', productRouter)
 
 app.listen(PORT, () => {
 console.log(`Server is listening on port ${PORT}`);
